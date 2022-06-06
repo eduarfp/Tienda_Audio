457cad491ca2eb1b870a87dd8c77974c765ee2f1
Eduardo <Eduardo@LAPTOP>
2022-06-06 00:45:25 -0600

    Creación Repositorio Tarea 2

---
+++ src/main/java/com/Tienda_Audio/TiendaAudioApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda_Audio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tienda_Audio/TiendaAudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda_Audio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaAudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
